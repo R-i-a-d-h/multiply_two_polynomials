89909e3537080857845c84d6c7abfbfbb71668d9
R-i-a-d-h <riadhaoumeur01@gmail.com>
2023-03-09 15:26:12 +0100

    add Exponent And Coefficient model

---
+++ lib/models/exponent_and_coefficient.dart
@@ -0,0 +1,11 @@
+class ExponentAndCoefficient {
+  late int exponent;
+  late int coefficient;
+
+  ExponentAndCoefficient(this.exponent, this.coefficient);
+
+  @override
+  String toString() {
+    return 'ExponentAndCoefficient{exponent: $exponent, coefficient: $coefficient}';
+  }
+}
