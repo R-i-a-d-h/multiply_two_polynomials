070af3ccf98477dcf5da50cb13111b703b15af50
R-i-a-d-h <riadhaoumeur01@gmail.com>
2023-03-11 15:23:33 +0100

    add Named constructor for Coefficient && Exponent

--- lib/models/exponent_and_coefficient.dart
+++ lib/models/exponent_and_coefficient.dart
@@ -4,6 +4,12 @@ class ExponentAndCoefficient {
 
   ExponentAndCoefficient(this.exponent, this.coefficient);
 
+  ExponentAndCoefficient.setExponent(this.exponent);
+
+  ExponentAndCoefficient.setCoefficient(this.coefficient);
+
+
+
   @override
   String toString() {
     return 'ExponentAndCoefficient{exponent: $exponent, coefficient: $coefficient}';
